9b811800be65544a354cc69cc47959abc1353f99
Paolo Rechia <paolorechia@gmail.com>
2018-10-16 06:15:45 -0300

    Manual compilation makes it work

--- todo_m/App.tsx
+++ todo_m/App.tsx
@@ -8,7 +8,8 @@
 
 import React from 'react';
 import {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput}
+from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,14 +18,80 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+interface TodoItem {
+  id: number,
+  name: string,
+  finished: boolean,
+}
+
+interface ItemList extends Array<TodoItem> {
+}
+
 type Props = {};
-export default class App extends Component<Props> {
+type State = {todo_list: ItemList};
+
+export default class App extends Component<Props, State> {
+  private todo_list: ItemList;
+  private currentId: number;
+  constructor(props: any){
+
+    super(props);
+    this.currentId = 0;
+    this.todo_list = [];
+    this.state = {
+      todo_list: this.todo_list,
+    }
+  }
+  public addItem(new_item: TodoItem){
+    this.setState((state, props) => {
+      this.todo_list.push(new_item);
+      return {todo_list: this.state.todo_list};
+    });
+  }
+  public removeItem(item_id: number){
+    this.setState((state, props) => {
+      console.log(this.state.todo_list);
+      let list = this.state.todo_list;
+      for (let i = 0; i < list.length; i++){
+        if (list[i].id == item_id){
+          this.state.todo_list.splice(i, 1);
+        }
+      }
+      return {todo_list: this.state.todo_list};
+    });
+  }
+  public finishItem(item_id: number){
+    this.setState((state, props) => {
+      let list = this.state.todo_list;
+      for (let i = 0; i < list.length; i++){
+        if (list[i].id == item_id)
+          this.state.todo_list[i].finished = true;
+      }
+      return {todo_list: this.state.todo_list};
+    });
+  }
+  public undoItem(item_id: number){
+    this.setState((state, props) => {
+      let list = this.state.todo_list;
+      for (let i = 0; i < list.length; i++){
+        if (list[i].id == item_id)
+        this.state.todo_list[i].finished = false;
+      }
+      return {todo_list: this.state.todo_list};
+    });
+  }
+  public textInputHandler(text: string){
+
+    return null;
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Task Manager!</Text>
+        <TextInput style={styles.input}
+                   placeholder="Type task here"
+                   onSubmitEditing={(text) => this.inputTextHandler(text)}
+        />
       </View>
     );
   }
@@ -46,5 +113,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  input: {
+    textAlign: 'center',
   },
 });
--- todo_m/index.js
+++ todo_m/index.js
@@ -1,7 +1,9 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './App.tsx'
+//import {Hello} from './components/Hello.tsx';
 import {name as appName} from './app.json';
 
+console.log(App);
 AppRegistry.registerComponent(appName, () => App);
